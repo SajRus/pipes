3821e18492affbcb660df1d38c1925c2509ff362
sajmir.rusi <sajmir.rusi@aizoon.it>
2017-06-12 10:32:15 +0200

    ready for lesson 6, about http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  serverName = '';
+  serverStatus = 'stable';
+
   appStatus = new Promise(
     (resolve, reject) => {
       setTimeout( () => {
@@ -16,25 +20,25 @@ export class AppComponent {
   filterStatus = '';
   servers = [
     {
-      instanceType: 'medium',
+      id: this.generateId(),
       name: 'Production Server',
       status: 'stable',
       started: new Date(2017, 6, 1)
     },
     {
-      instanceType: 'large',
+      id: this.generateId(),
       name: 'User Database',
       status: 'stable',
       started: new Date(2017, 6, 2)
     },
     {
-      instanceType: 'small',
+      id: this.generateId(),
       name: 'Development Server',
       status: 'offline',
       started: new Date(2017, 6, 3)
     },
     {
-      instanceType: 'small',
+     id: this.generateId(),
       name: 'Testing Environment Server',
       status: 'stable',
       started: new Date(2017, 6, 4)
@@ -43,17 +47,21 @@ export class AppComponent {
 
   onAddServer(){
     this.servers.push({
-      instanceType: 'small',
-      name: 'Test Aizoon Server',
-      status: 'offline',
+      id: this.generateId(),
+      name: this.serverName,
+      status: this.serverStatus,
       started: new Date(2017, 6, 9)
     })
   }
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+  getStatusClasses(server: {instanceType: number, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  private generateId(){
+    return Math.round(Math.random() * 10000);
   }
 }
